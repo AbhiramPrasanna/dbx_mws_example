332a05ee45d802f89dea6023355d07613dd0443d
AbhiramPrasanna <103725809+AbhiramPrasanna@users.noreply.github.com>
2025-01-26 05:49:16 +0530

    Update provider.tf

--- repo/common_modules_account/workspace_creation/provider.tf
+++ repo/common_modules_account/workspace_creation/provider.tf
@@ -3,5 +3,18 @@ terraform {
     databricks = {
       source = "databricks/databricks"
     }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "databricks" {
+  host  = var.databricks_host
+  token = var.databricks_token
 }
