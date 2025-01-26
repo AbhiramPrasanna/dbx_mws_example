06743ba0e211134c05edb575436bdbb6fc980e1e
AbhiramPrasanna <103725809+AbhiramPrasanna@users.noreply.github.com>
2025-01-26 05:52:00 +0530

    Update variables.tf

--- repo/common_modules_account/workspace_creation/variables.tf
+++ repo/common_modules_account/workspace_creation/variables.tf
@@ -1,31 +1,11 @@
-variable "databricks_account_id" {
-  type        = string
+variable "resource_group_name" {
+  type = string
 }
 
-variable "region" {
-  type        = string
-}
-
-variable "cross_account_role_arn" {
-  type        = string
+variable "location" {
+  type = string
 }
 
 variable "resource_prefix" {
-  type        = string
-}
-
-variable "bucket_name" {
-  type        = string
-}
-
-variable "security_group_ids" {
-  type        = list(any)
-}
-
-variable "subnet_ids" {
-  type        = list(any)
-}
-
-variable "vpc_id" {
-  type        = string
+  type = string
 }
