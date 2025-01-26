5285a7521f3e317b151b3a0f9a5fe6afd938144d
AbhiramPrasanna <103725809+AbhiramPrasanna@users.noreply.github.com>
2025-01-26 05:43:44 +0530

    Update provider.tf

--- repo/common_modules_account/identity_assignment/provider.tf
+++ repo/common_modules_account/identity_assignment/provider.tf
@@ -3,5 +3,13 @@ terraform {
     databricks = {
       source = "databricks/databricks"
     }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
